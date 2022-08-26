a922fc55ae0a3bcdb48e9bc4a7133826b444b71c
edson-sp <greegrocha48@gmail.com>
2022-08-25 22:47:27 -0300

    [Fix][update]

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,17 +1,30 @@
-export class ContacCorrente {
+export class ContaCorrente {
     agencia;
-    saldo;
+    cliente;
+
+
+    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo = valor;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
         }
     }
+
     depositar(valor) {
-        if (valor > 0) {
-            this.saldo += valor;
+        if (valor <= 0) {
+            return;
         }
+        this._saldo += valor;
     }
 
+    tranferir(valor, conta) {
 
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+    }
 }
