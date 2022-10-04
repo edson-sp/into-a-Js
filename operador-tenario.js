4c3ca5e8b171401d2f22b46781e026344859197a
Edson-sp <89952552+edson-sp@users.noreply.github.com>
2022-10-03 21:54:58 -0300

    Create operador-tenario.js

---
+++ operador-tenario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 17;
+
+// if (idadeCliente >= idadeMinima) {
+// console.log("cerveja")
+// } else {
+// console.log("refri")
+// }
+
+console.log(idadeMinima >= idadeCliente ? "refri" : "cereja")
